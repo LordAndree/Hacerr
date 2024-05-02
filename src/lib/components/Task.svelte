2c04efb855195f573b6b3b3a1d76e77c7d6061d6
Mambaul Hisam <hisyamsquartz@gmail.com>
2024-05-03 02:32:08 +0700

    update dashboard

---
+++ src/lib/components/Task.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Box from "./core/Box.svelte";
+	import Button from "./core/Button.svelte";
+
+    export let tgl;
+</script>
+<div style="display: flex; justify-content: space-between; width: 100%;">
+    <Box>
+        <p style="margin: 0;">{tgl}</p>
+    </Box>
+    <Button alias="edit" href="#"></Button>
+</div>
+<Box>
+    <div style="width: 47vw; max-width: 47vw;">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit similique quasi asperiores deserunt nesciunt, illum et labore nisi optio exercitationem dolore iusto minus quod, voluptatum excepturi dolorum veritatis reprehenderit? Ab id amet ex nam odio fuga fugiat neque odit? Adipisci in similique sapiente, ipsam minus excepturi fugiat impedit voluptas deserunt commodi veritatis neque nemo atque, nostrum velit dolor quisquam aut deleniti quo molestias praesentium laborum delectus, voluptates dignissimos. Voluptatum facilis quis laudantium culpa officiis harum explicabo provident vitae! Eos ipsum perferendis nam similique hic nihil consequuntur ducimus aliquid nesciunt repudiandae, eligendi voluptate assumenda inventore quo nemo sequi doloribus! Consequatur, ex!
+        </p>
+    </div>
+</Box>
+<div style="padding-bottom: 0.5vw;"></div>
+<hr style="width: 100%; height: 2px; background-color: black;">
+<div style="padding-bottom: 0.5vw;"></div>
