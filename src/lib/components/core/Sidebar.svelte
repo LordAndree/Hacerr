150f552267b710277547d899242661fb248c6a1a
LordAndree <andreasadrian46@gmail.com>
2024-05-02 22:02:20 +0700

    dashboard v1

---
+++ src/lib/components/core/Sidebar.svelte
@@ -0,0 +1,36 @@
+<script>
+    // Tidak diperlukan logika khusus di sini
+</script>
+
+
+<style>
+    .container2{
+        /* border: solid; */
+        background-color: white;
+        flex-direction: column;
+        width: 20%;
+        height: none;
+        border-radius: 3%;
+        margin: 10% ;
+        margin: 1%;
+        padding: 1%;
+    }
+    .navbutton{
+        background-color: #E2E7FA;
+        border-radius: 7%;
+        border: solid;
+        font-family: 'Courier New', Courier, monospace;
+        margin: 3%;
+    }
+    </style>
+
+
+<div class="container2">
+    <h2 style="font-family: 'Courier New', Courier, monospace;">Hello User</h2>
+    <div class="navbutton">
+        <h1>Home</h1>
+    </div>
+    <div class="navbutton">
+        <h1>Profile</h1>
+    </div>
+</div>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,7 +1,32 @@
 <script>
 	import Nav from '$lib/components/Nav.svelte';
 	import NavTest from '$lib/components/NavTest.svelte';
-</script>
+	import Sidebar from '$lib/components/core/Sidebar.svelte';
+	import TaskCard from '$lib/components/core/TaskCard.svelte';
+	import TaskList from '$lib/components/core/TaskList.svelte';
 
-<Nav></Nav>
-<NavTest></NavTest>
+</script>
+<style>
+		.container1{
+			display: flex;
+			flex-direction: row;
+			justify-content: space-around;
+			/* border: solid; */
+			border-radius: 1%;
+			justify-self: center;
+			margin: auto;
+			width: 90%;
+			background-color: #515173;
+			padding: 1%;
+		}
+</style>
+
+<h1 style="text-align: center; margin: 2%;">HACER</h1>
+
+<div class="container1">
+	<Sidebar></Sidebar>
+	<TaskList>
+	</TaskList>
+</div>
+
+
